e6381669bbbcd773d42f9636cc7ebdd74ac5ad81
Devin Mork <devin.mork@gmail.com>
2017-12-15 22:47:16 -0600

    Completed bullet_point_adder.py for ch6 exercises.

---
+++ ch6/bullet_pt_adder/bullet_point_adder.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+# bullet_pont_adder.py - Adds Wikipedia bullet points to the start
+# of each line of text on the clipboard.
+
+import pyperclip
+text = pyperclip.paste()
+
+# TODO: Separate lines and add starts.
+
+# Separate lines and add stars.
+lines = text.split('\n')
+for i in range(len(lines)):     # loop through all indexes in the "lines" list
+    lines[i] = '* ' + lines[i]  # add star to each string in "lines" list
+text = '\n'.join(lines)
+
+pyperclip.copy(text)
