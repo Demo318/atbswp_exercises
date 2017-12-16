ba5d3935ed5a7a55ce5b902a826e2d7eacf20f8c
Devin Mork <devin.mork@gmail.com>
2017-12-15 23:54:21 -0600

    Linted the crap out of print_table.py

--- ch6/print_table/print_table.py
+++ ch6/print_table/print_table.py
@@ -1,20 +1,24 @@
-tableData = [['apples', 'oranges', 'cherries', 'banana'],
-             ['Alice', 'Bob', 'Carol', 'David'],
-             ['dogs', 'cats', 'moose', 'goose']]
+"""Does this count as the module docstring?"""
+
+TABLE_DATA = [['apples', 'oranges', 'cherries', 'banana'],
+              ['Alice', 'Bob', 'Carol', 'David'],
+              ['dogs', 'cats', 'moose', 'goose']]
 
 def print_table(table):
+    """Print 2-dimensional array with all items evenly organized into columns"""
     column_widths = find_column_widths(table)
     table = reformat_items(table, column_widths)
     for i in table:
         print('  '.join(i))
-    
+    return True
 
 def find_column_widths(table):
+    """Identify the longest string length for each column."""
     max_widths = [0]* len(table[0])
 
-    for i in range(len(table)):
-        for j in range(len(table[i])):
-            temp_width = len(table[i][j])
+    for i in enumerate(table):
+        for j, k in enumerate(table[i[0]]):
+            temp_width = len(k)
             current_width = max_widths[j]
             if temp_width > current_width:
                 max_widths[j] = temp_width
@@ -22,10 +26,11 @@ def find_column_widths(table):
 
 
 def reformat_items(table, widths):
-    for i in range(len(table)):
-        for j in range(len(table[i])):
-            table[i][j] = table[i][j].ljust(widths[j])
+    """Add white space so every string in column has same length."""
+    for i in enumerate(table):
+        for j, k in enumerate(table[i[0]]):
+            table[i[0]][j] = k.ljust(widths[j])
     return table
 
 
-print_table(tableData)
+print_table(TABLE_DATA)
