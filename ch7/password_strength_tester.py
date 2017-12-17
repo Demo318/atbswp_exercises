8f76f63a21b9319885e02e36d6b2ee822b61dc89
Devin Mork <devin.mork@gmail.com>
2017-12-17 14:42:18 -0600

    Finish password_strength_tester.py from ch7.

--- ch7/password_strength_tester.py
+++ ch7/password_strength_tester.py
@@ -6,38 +6,72 @@
 import re
 
 def test_password():
+    """
+    Function to test if password meets all requirements.
+    Provide user with feedback on what's missing.
+    """
     password = input('Please enter a password:')
     missing_items = []
 
     if not lower_case_ltr(password):
-        missing_items += 'lowercase letter'
+        missing_items.append('lowercase letter')
     if not upper_case_ltr(password):
-        missing_items += 'uppercase letter'
+        missing_items.append('uppercase letter')
     if not digit(password):
-        missing_items += 'digit'
+        missing_items.append('digit')
     if not symbol(password):
-        missing_items += 'symbol'
+        missing_items.append('symbol')
 
-    if len(missing_items) > 0:
+    if missing_items > 0:
         print("Your password is missing:")
         for i in enumerate(missing_items):
             print(str(i[1]))
-        
-    
+    else:
+        print("Your password has good complexity.")
 
+    if not length(password):
+        print("Your password needs to be at least eight characters long.")
+    else:
+        print("Your password is plenty long enough.")
 
-"""Checks string to see if it contains a lowercase letter."""
 def lower_case_ltr(text):
+    """Checks string to see if it contains a lowercase letter."""
+    lcl_regex = re.compile("[a-z]")
+    if lcl_regex.search(text):
+        return True
+    else:
+        return False
 
-
-"""Checks string to see if it contains an uppercase letter."""
 def upper_case_ltr(text):
+    """Checks string to see if it contains an uppercase letter."""
+    ucl_regex = re.compile("[A-Z]")
+    if ucl_regex.search(text):
+        return True
+    else:
+        return False
 
-"""Checks string to see if it contains a digit."""
 def digit(text):
+    """Checks string to see if it contains a digit."""
+    d_regex = re.compile(r"\d")
+    if d_regex.search(text):
+        return True
+    else:
+        return False
 
-"""Checks string to see if it contains a symbol.""""
 def symbol(text):
+    """Checks string to see if it contains a symbol."""
+    s_regex = re.compile(r"\D\W\S")
+    if s_regex.search(text):
+        return True
+    else:
+        return False
 
-"""checks string to see if it is at least eight carachters long.""""
 def length(text):
+    """checks string to see if it is at least eight carachters long."""
+    if len(text) < 8:
+        return False
+    else:
+        return True
+
+
+test_password()
