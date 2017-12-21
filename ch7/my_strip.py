fb56e4d2ce40c84f5e62bd8cd49d2bd7bba0c559
Devin Mork <devin.mork@gmail.com>
2017-12-21 13:58:36 -0600

    Lint the crap out of my_strip.py

--- ch7/my_strip.py
+++ ch7/my_strip.py
@@ -1,57 +1,47 @@
 #! /usr/bin/env python3
+# pylint: disable=no-member
 # Create own version of 'strip' method using regexp.
 
-
+"""Exercise from ch7 of How to Automate the Boring Stuff in Python."""
 
 import re
 
-def my_strip(usrstring, chars = " "):
+def my_strip(usrstring, chars=" "):
     """Method to copy functionality of strip() built-in string method."""
 
     # identify special characters in chars variable
-    chars_list= list(chars)
+    chars_list = list(chars)
     special_chars_reg = re.compile(r"\W")
-    for i, c in enumerate(chars_list):
-        if special_chars_reg.search(c):
-            chars_list[i] = '\\' + c
-
+    for i, j in enumerate(chars_list):
+        if special_chars_reg.search(j):
+            chars_list[i] = '\\' + j
 
     # Take chars to create regex.
-
     chars_with_pipes = "|".join(chars_list)
     regex = re.compile(chars_with_pipes)
 
-
-    # Loop from beginning of string
-    # Store index from beginning
-    # When matches no longer found, break loop & delete indeces that were matched.
+    # Start from beginning, find first character that doesn't match chars
     pos_index = 0
-    for i, x in enumerate(list(usrstring)):
-        if regex.search(x):
+    for i, j in enumerate(list(usrstring)):
+        if regex.search(j):
             pos_index = i + 1
         else:
             break
-   
-    # Loop from end of string, counting down i.e. -1, -2, etc.
-    # Store index as negative value. 
-    # When matches no longer found, break loop & delte indeces that were matched.
+
+    # Start from end, find first character that doesn't match chars
     neg_index = None
-    for i,x in enumerate(list(reversed(usrstring))):
-        if regex.search(x):
+    for i, j in enumerate(list(reversed(usrstring))):
+        if regex.search(j):
             neg_index = -i - 1
         else:
             break
 
     # Return updated string with characters removed.
-
     final_string = usrstring[pos_index:neg_index]
-    return(final_string)
+    return final_string
 
 print("Tests:")
 print(str(my_strip("  test  ") == "test"))
-
 print(str(my_strip("...test...", ".") == "test"))
-
 print(str(my_strip("awsdtestawsd", "awsd") == "test"))
-
 print(str(my_strip("sdtestaw", "awsd") == "test"))
