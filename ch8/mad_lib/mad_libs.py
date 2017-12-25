3305d5f0865961d9ee864772751c23b38b8b16ae
Devin Mork <devin.mork@gmail.com>
2017-12-24 23:20:52 -0600

    Got loop working.

--- ch8/mad_lib/mad_libs.py
+++ ch8/mad_lib/mad_libs.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python3
 # Fantasy game inventory exercise from 'Automate the Boring Stuff with Python' ch.5
 # https://automatetheboringstuff.com/chapter8/
+# written using Python 3.6.3
+
 
 import re
 import random
@@ -20,39 +22,43 @@ for line in enumerate(libs_file.readlines()):
     if not re.search(r'^--', line[1]):
         libs.append(line[1])
 
-# Recursive method to find 
-def find_random_number(max, history):
-    rand_int = random.randint(0, max)
-    while rand_int in history:
-        find_random_number(max, history)
-    return rand_int
+# Begin loop, allowing player to cycle through lots
+# of mad libs in one session if they choose.
+is_playing = True
+while is_playing:
 
 # Clear already_used_libs list if player has already gone through
-# the entire inventory.
-if len(libs) == len(already_used_libs):
-    already_used_libs = []
+    # the entire inventory.
+    if len(libs) == len(already_used_libs):
+        already_used_libs = []
 
-# Use find_random_number to determine which madlib is next.
-lib_ind = find_random_number(len(libs) - 1, already_used_libs)
+    # Determine which madlib is next.
+    lib_ind = random.randint(0, len(libs) - 1)
+    while lib_ind in already_used_libs:
+        lib_ind = random.randint(0, len(libs) - 1)
 
-# Record randomly generated lib_ind to madlib already-used history.
-already_used_libs.append(lib_ind)
+    # Record randomly generated lib_ind to madlib already-used history.
+    already_used_libs.append(lib_ind)
 
-# Create regex object.
-regd_lib = lib_regex.search(libs[lib_ind])
+    # Create regex object.
+    regd_lib = lib_regex.search(libs[lib_ind])
 
-# Separate the madlib string from the input needed from user.
-lib_string = regd_lib.group(2)
-lib_words = regd_lib.group(4).split(", ")
+    # Separate the madlib string from the input needed from user.
+    lib_string = regd_lib.group(2)
+    lib_words = regd_lib.group(4).split(", ")
 
-# Record user-entries for variable fields.
-user_entries = []
-for word in enumerate(lib_words):
-    print("Please enter a {}:".format(word[1]))
-    user_entries.append(input())
+    # Record user-entries for variable fields.
+    user_entries = []
+    for word in enumerate(lib_words):
+        print("Please enter a {}:".format(word[1]))
+        user_entries.append(input())
 
-# Print string back to the console for user.
-print(lib_string.format(*user_entries).replace('\\n','\n'))
+    # Print string back to the console for user.
+    print(lib_string.format(*user_entries).replace('\\n','\n'))
 
+    # Ask user if they would like to play again.
+    print(" ")
+    print("Would you like another one? (y/n)")
+    if input() == 'n':
+        is_playing = False
 
-# TODO: Package code in a loop so player can keep playing.
